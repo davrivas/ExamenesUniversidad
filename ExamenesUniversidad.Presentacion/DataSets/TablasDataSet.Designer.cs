468d1b6d85431b0c3e0390fa88975b31d1cd38bf
David Rivas <d.rivas95@hotmail.com>
2019-06-04 23:08:49 -0500

    Cambios en el dataset y en el formulario de profesor (falta hacer procedimientos de almacenado o vistas)

--- ExamenesUniversidad.Presentacion/DataSets/TablasDataSet.Designer.cs
+++ ExamenesUniversidad.Presentacion/DataSets/TablasDataSet.Designer.cs
@@ -4030,11 +4030,15 @@ namespace ExamenesUniversidad.Presentacion.DataSets.TablasDataSetTableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT Id, Codigo, Nombre FROM dbo.Cursos";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT Id, Codigo, Nombre FROM dbo.Cursos ORDER BY Nombre";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -4059,6 +4063,19 @@ namespace ExamenesUniversidad.Presentacion.DataSets.TablasDataSetTableAdapters {
             TablasDataSet.CursosDataTable dataTable = new TablasDataSet.CursosDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "16.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBy(TablasDataSet.CursosDataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
--- ExamenesUniversidad.Presentacion/Profesor/InicioProfesor.Designer.cs
+++ ExamenesUniversidad.Presentacion/Profesor/InicioProfesor.Designer.cs
@@ -31,7 +31,16 @@
             this.tabControlProfesor = new System.Windows.Forms.TabControl();
             this.tabPageCursos = new System.Windows.Forms.TabPage();
             this.tabPageExamenes = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fillByToolStrip = new System.Windows.Forms.ToolStrip();
+            this.fillByToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.tabControlProfesor.SuspendLayout();
+            this.tabPageCursos.SuspendLayout();
+            this.tabPageExamenes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.fillByToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControlProfesor
@@ -46,6 +55,7 @@
             // 
             // tabPageCursos
             // 
+            this.tabPageCursos.Controls.Add(this.dataGridView1);
             this.tabPageCursos.Location = new System.Drawing.Point(4, 22);
             this.tabPageCursos.Name = "tabPageCursos";
             this.tabPageCursos.Padding = new System.Windows.Forms.Padding(3);
@@ -56,24 +66,73 @@
             // 
             // tabPageExamenes
             // 
+            this.tabPageExamenes.Controls.Add(this.dataGridView2);
             this.tabPageExamenes.Location = new System.Drawing.Point(4, 22);
             this.tabPageExamenes.Name = "tabPageExamenes";
             this.tabPageExamenes.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageExamenes.Size = new System.Drawing.Size(553, 74);
+            this.tabPageExamenes.Size = new System.Drawing.Size(553, 240);
             this.tabPageExamenes.TabIndex = 1;
             this.tabPageExamenes.Text = "Examenes";
             this.tabPageExamenes.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(553, 240);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fillByToolStrip
+            // 
+            this.fillByToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fillByToolStripButton});
+            this.fillByToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fillByToolStrip.Name = "fillByToolStrip";
+            this.fillByToolStrip.Size = new System.Drawing.Size(800, 25);
+            this.fillByToolStrip.TabIndex = 1;
+            this.fillByToolStrip.Text = "fillByToolStrip";
+            this.fillByToolStrip.Visible = false;
+            // 
+            // fillByToolStripButton
+            // 
+            this.fillByToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillByToolStripButton.Name = "fillByToolStripButton";
+            this.fillByToolStripButton.Size = new System.Drawing.Size(39, 22);
+            this.fillByToolStripButton.Text = "FillBy";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(553, 240);
+            this.dataGridView2.TabIndex = 0;
             // 
             // InicioProfesor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fillByToolStrip);
             this.Controls.Add(this.tabControlProfesor);
             this.Name = "InicioProfesor";
             this.Text = "Profesor inicio";
             this.tabControlProfesor.ResumeLayout(false);
+            this.tabPageCursos.ResumeLayout(false);
+            this.tabPageExamenes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.fillByToolStrip.ResumeLayout(false);
+            this.fillByToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +141,9 @@
         private System.Windows.Forms.TabControl tabControlProfesor;
         private System.Windows.Forms.TabPage tabPageCursos;
         private System.Windows.Forms.TabPage tabPageExamenes;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStrip fillByToolStrip;
+        private System.Windows.Forms.ToolStripButton fillByToolStripButton;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
