c8d65d28fa635bc273263cdc3cf55abde75b7a94
David Rivas <d.rivas95@hotmail.com>
2019-06-05 19:05:36 -0500

    Crear Configuration de la base de datos

---
+++ ExamenesUniversidad.Datos/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ExamenesUniversidad.Datos.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExamenesUniversidad.Datos.DbContexts.ExamenesUniversidadContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ExamenesUniversidad.Datos.DbContexts.ExamenesUniversidadContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
