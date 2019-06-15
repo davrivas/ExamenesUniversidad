fce625febd4f5815a2c3a92041d3511bab0a4188
David Rivas <d.rivas95@hotmail.com>
2019-06-14 20:01:53 -0500

    Nuevo DTO para resultado

---
+++ ExamenesUniversidad.Datos/DTOs/EstudianteDTOs/EstudianteResultadoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenesUniversidad.Datos.DTOs.EstudianteDTOs
+{
+    public class EstudianteResultadoDTO
+    {
+        public string CodigoExamen { get; set; }
+        public string NombreCurso { get; set; }
+        public string NombreProfesor { get; set; }
+        public string Resultado { get; set; }
+    }
+}
---
+++ ExamenesUniversidad.Datos/DTOs/ProfesorDTOs/ExamenResultadoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenesUniversidad.Datos.DTOs.ProfesorDTOs
+{
+    public class ExamenResultadoDTO
+    {
+        public string CodigoExamen { get; set; }
+        public string NombreCurso { get; set; }
+        public string NombreEstudiante { get; set; }
+        public int NumeroCarnet { get; set; }
+        public string Resultado { get; set; }
+    }
+}
